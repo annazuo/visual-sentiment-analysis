78e1adf05efa0fa6447d724bf199cae2700d3008
Jess Bunnag <tb2663@columbia.edu>
2023-05-02 17:20:17 -0400

    add comments to parse_predictions

--- parse_predictions.py
+++ parse_predictions.py
@@ -3,13 +3,17 @@ import pandas as pd
 import sys
 
 def main(file_name):
+    # read the probability prediction from a file
     pred = pd.read_csv(file_name)
     pred_tensor = torch.tensor(pred.values)
 
+    # get the argmax prediction of each frame from the scores 
     pred_argmax = pred_tensor.argmax(dim=1)
 
+    # get the prediction count frequency from all the frames
     unique, count = pred_argmax.unique(return_counts=True)
 
+    # get the argmax prediction of each video
     max_pred = unique[count.argmax()].item()
 
     if max_pred == 0:
@@ -18,8 +22,6 @@ def main(file_name):
         print("NEUTRAL")
     elif max_pred == 2:
         print("POSITIVE")
-
-
 
 
 if __name__ == '__main__':
